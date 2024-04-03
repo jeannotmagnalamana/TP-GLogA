08a7df447af70dd51480441e64a72afa21b61bb4
jeannotmagnalamana <magnalamanajeannot@gmail.com>
2024-04-03 20:26:17 +0200

    Modification classe Homme et Femme

--- Test_unitaire/src/iut/bad/Femme.java
+++ Test_unitaire/src/iut/bad/Femme.java
@@ -1,42 +1,11 @@
 package iut.bad;
 
-public class Femme {
-    
-    private String nom;
-    private String prenom;
-    private int age;
+public class Femme extends Humain {
     
     public Femme() {
     }
     
     public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+        super(nom, prenom, age); 
     }
 }
-
--- Test_unitaire/src/iut/bad/Homme.java
+++ Test_unitaire/src/iut/bad/Homme.java
@@ -1,41 +1,11 @@
 package iut.bad;
 
-public class Homme {
-	
-    private String nom;
-    private String prenom;
-    private int age;
+public class Homme extends Humain {
     
     public Homme() {
     }
     
     public Homme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+        super(nom, prenom, age);
     }
 }
