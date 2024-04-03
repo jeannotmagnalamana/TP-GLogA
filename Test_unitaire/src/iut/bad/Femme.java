22c489cc59600b1ffab0bf0733c02f169eee34f7
jeannotmagnalamana <magnalamanajeannot@gmail.com>
2024-04-03 21:29:35 +0200

    Methode main

--- Test_unitaire/src/iut/bad/Femme.java
+++ Test_unitaire/src/iut/bad/Femme.java
@@ -8,9 +8,18 @@ public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
-
+    
     @Override
     public String toString() {
         return "Femme: " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Femme femme = new Femme("Doe", "Jane", 28);
+        Homme homme = new Homme("Smith", "John", 30);
+        
+        femme.ami(homme);
+        
+        System.out.println(femme.getNom() + " est ami avec " + homme.getNom());
     }
 }
